271dfc8d5397343f29948d5f1534326b2c1d5759
Maximad <aboudiab@gmail.com>
2025-11-25 18:31:48 +0300

    Refresh channel map resolution on lookup

--- src/discord/utils/channels.js
+++ src/discord/utils/channels.js
@@ -1,29 +1,32 @@
 const cfg = require('../../../config.json');
 
-const productionChannels = cfg.channels?.production || cfg.production || {};
-const mediaChannels = cfg.channels?.media || cfg.media || {};
+function getChannelMap() {
+  const productionChannels = cfg.channels?.production || cfg.production || {};
+  const mediaChannels = cfg.channels?.media || cfg.media || {};
 
-const channelMap = {
-  'production.crew_roster': productionChannels.crewRosterId,
-  'production.gear_log': productionChannels.gearLogId,
-  'production.post_pipeline': productionChannels.postPipelineId,
-  'production.location': productionChannels.gearLogId,
-  'production.tests': productionChannels.postPipelineId,
-  'production.post_mortem': productionChannels.postPipelineId,
-  'production.archive': productionChannels.postPipelineId,
-  'production.sound_library': productionChannels.postPipelineId,
-  'media.assignments': mediaChannels.assignmentsId,
-  'media.edits': mediaChannels.editsId,
-  'media.fact_check': mediaChannels.factCheckId,
-  'media.photo': mediaChannels.photoId,
-  'media.video': mediaChannels.videoId,
-  'media.graphics': mediaChannels.graphicsId,
-  'media.sound': mediaChannels.soundId,
-  'media.exports': mediaChannels.exportsId,
-  'admin.emergency': null
-};
+  return {
+    'production.crew_roster': productionChannels.crewRosterId,
+    'production.gear_log': productionChannels.gearLogId,
+    'production.post_pipeline': productionChannels.postPipelineId,
+    'production.location': productionChannels.gearLogId,
+    'production.tests': productionChannels.postPipelineId,
+    'production.post_mortem': productionChannels.postPipelineId,
+    'production.archive': productionChannels.postPipelineId,
+    'production.sound_library': productionChannels.postPipelineId,
+    'media.assignments': mediaChannels.assignmentsId,
+    'media.edits': mediaChannels.editsId,
+    'media.fact_check': mediaChannels.factCheckId,
+    'media.photo': mediaChannels.photoId,
+    'media.video': mediaChannels.videoId,
+    'media.graphics': mediaChannels.graphicsId,
+    'media.sound': mediaChannels.soundId,
+    'media.exports': mediaChannels.exportsId,
+    'admin.emergency': null
+  };
+}
 
 function getChannelIdByKey(channelKey) {
+  const channelMap = getChannelMap();
   return channelMap[channelKey] || null;
 }
 
